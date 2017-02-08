7f3f39860231238637c9a92ded756473c99e4200
pacoggg <franciscoggomez@gmail.com>
2017-02-08 01:47:28 +0100

    Implementados modulos de añadir cliente y listar clientes

---
+++ Empresa/src/empresa/modulos.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresa;
+
+import java.io.*;
+
+/**
+ *
+ * @author Montse
+ */
+public class modulos {
+   
+   private String nif;
+   private String nombre;
+   private String telefono;
+   private String direccion;
+   private String deuda;
+
+    public modulos() {
+    }
+
+    public modulos(String nif, String nombre, String telefono, String direccion, String deuda) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.deuda = deuda;
+    }
+    
+   public void escribir() throws IOException{
+    File f;
+    FileWriter fich;
+    BufferedWriter bw;
+    PrintWriter pw;
+    f = new File("clientes.dat");
+    fich = new FileWriter(f,true);
+    bw= new BufferedWriter(fich);
+    pw = new PrintWriter(bw);
+    pw.write("NIF: "+nif+" "+"NOMBRE: "+nombre+" "+"TELEFONO: "+telefono+" "+"DIRECCION: "+direccion+" "+"DEUDA: "+deuda+"\n");
+    pw.close();
+    bw.close();
+    //nif+"%"+nombre+"%"+telefono+"%"+direccion+"%"+deuda+"/n"
+   }
+   public void listar() throws IOException{
+       File f;
+       FileReader fr;
+       BufferedReader br;
+       
+       f= new File("clientes.dat");
+       fr=new FileReader(f);
+       br=new BufferedReader(fr);
+       String linea;
+       while((linea=br.readLine())!=null){
+           System.out.println(linea); 
+       }
+       br.close();
+       fr.close();
+   }
+   
+}
